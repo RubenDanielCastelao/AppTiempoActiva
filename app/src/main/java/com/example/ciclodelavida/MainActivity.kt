2678424a7233ca9952ba203d914a0e1ccd218adf
RubenDanielCastelao <rnunezgonzalez@danielcastelao.org>
2023-10-04 10:28:49 +0200

    Añadido boton de numero aleatorio

--- app/src/main/java/com/example/ciclodelavida/MainActivity.kt
+++ app/src/main/java/com/example/ciclodelavida/MainActivity.kt
@@ -7,14 +7,27 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ciclodelavida.ui.theme.CicloDeLaVidaTheme
 import com.example.ciclodelavida.Greeting as Greeting1
 
@@ -38,7 +51,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Gray
                 ) {
                     Greeting1("Android")
                 }
@@ -84,11 +97,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var _number = remember {mutableStateOf(0)}
+
     Text(
-        text = "Hola!",
-        modifier = modifier
+        text = "Numero: ${_number.value}",
+        color = Color.White
     )
+    Column(Modifier.fillMaxWidth().absolutePadding(10.dp, 200.dp, 10.dp, 0.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = { _number.value = (0..10).random() },
+            modifier = Modifier.padding(64.dp),
+
+            )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_numbers_24),
+                contentDescription = null,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "Generar numero",
+                color = Color.White
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
